e2ba256b6826912609cd74b8e0d447469ea025ff
adgvcxz <344292600@qq.com>
2017-06-05 17:43:34 +0800

    add RecyclerView

---
+++ recyclerviewmodel/src/main/java/com/adgvcxz/recyclerviewmodel/RecyclerViewModel.kt
@@ -0,0 +1,17 @@
+package com.adgvcxz.recyclerviewmodel
+
+import com.adgvcxz.IModel
+import com.adgvcxz.WidgetViewModel
+
+/**
+ * zhaowei
+ * Created by zhaowei on 2017/6/5.
+ */
+
+class RecyclerViewModel: WidgetViewModel<RecyclerViewModel.Model>(Model()) {
+
+    class Model: IModel {
+
+    }
+
+}
